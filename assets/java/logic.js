edb049e27d0321562a8712949b72778b7ee90b17
T4T4el7 <ochoaarmando13@gmail.com>
2019-01-17 20:50:16 -0800

    created the document on ready buttons and the button class, sill proceed to link the ajax call with button clicks

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -1,10 +1,28 @@
 //hello world
 
-var topics = ["connor mcgregor" , "jon jones" , "yoel romero" , "amanda nunes"];
 
 var test = "ufc";
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + test + "&api_key=lrBVvkogHvFM0M7SIt1N7bIGmqZMJ5ts&limit=10";
+
+
+
+$(document).ready(function() {
+    var topics = ["connor mcgregor" , "jon jones" , "yoel romero" , "amanda nunes" , "ufc"];
+
+    for(var i = 0; i < topics.length; i++) {
+        var strt = topics[i];
+        console.log(strt);
+
+        var strtBtn = $("<button>");
+
+        strtBtn.addClass("startBtn");
+        strtBtn.attr(strt);
+        strtBtn.text(strt);
+        $("#buttons").append(strtBtn);
+    };
+
+})
 
 $.ajax({
     url: queryURL,
