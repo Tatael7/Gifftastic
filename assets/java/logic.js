ca0f254d8c36d3ae30be67185f676c68cb03c37a
T4T4el7 <ochoaarmando13@gmail.com>
2019-01-17 21:42:21 -0800

    successfully created the buttons for the initial topics as well as attatching the ajax pull for those specific topics. will proceed to iscolate the ajax pull as a single function to be used in the on clicks of the added buttons. next will add buttons from the search bar

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -3,12 +3,15 @@
 
 var test = "ufc";
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + test + "&api_key=lrBVvkogHvFM0M7SIt1N7bIGmqZMJ5ts&limit=10";
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + test + "&api_key=lrBVvkogHvFM0M7SIt1N7bIGmqZMJ5ts&limit=10";
 
 
 
 $(document).ready(function() {
-    var topics = ["connor mcgregor" , "jon jones" , "yoel romero" , "amanda nunes" , "ufc"];
+
+    //creating the starting topic buttons 
+
+    var topics = ["connor mcgregor" , "jon jones" , "yoel romero" , "tj dillashaw" , "amanda nunes" , "ufc"];
 
     for(var i = 0; i < topics.length; i++) {
         var strt = topics[i];
@@ -16,17 +19,26 @@ $(document).ready(function() {
 
         var strtBtn = $("<button>");
 
-        strtBtn.addClass("startBtn");
-        strtBtn.attr(strt);
+        strtBtn.addClass("personBtn");
+        strtBtn.attr("data-person" , strt);
         strtBtn.text(strt);
         $("#buttons").append(strtBtn);
     };
 
-})
+    // on click that results in the ajax pull  of the data-person aka topic of the button
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
+    $(".personBtn").on("click", function() {
+        var cosa = $(this).attr("data-person");
+        console.log(cosa);
+        
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cosa + "&api_key=lrBVvkogHvFM0M7SIt1N7bIGmqZMJ5ts&limit=10";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+        });
+    });
 });
+
