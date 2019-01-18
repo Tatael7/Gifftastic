97b53a2ac7065568aa672f3c1e18e5a0687e5aeb
T4T4el7 <ochoaarmando13@gmail.com>
2019-01-17 20:00:46 -0800

    have created the GET command in js as well as the search bar, will now proceed to create the onclick function that will be connected to the search button

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -1,1 +1,14 @@
-// hello world
+//hello world
+
+var topics = ["connor mcgregor" , "jon jones" , "yoel romero" , "amanda nunes"];
+
+var test = "ufc";
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + test + "&api_key=lrBVvkogHvFM0M7SIt1N7bIGmqZMJ5ts&limit=10";
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
